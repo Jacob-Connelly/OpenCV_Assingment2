dc5091aa07838458947941b44c21a1f354b76bd7
Jacob Connelly <70172838+Jacob-Connelly@users.noreply.github.com>
2021-02-18 21:52:01 -0700

    Commented code for better readability. Project done.

--- Part1.py
+++ Part1.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import numpy as np
 
-
+# Global variables to hold high and low values
 global hH
 global hL
 global sH
@@ -10,6 +10,7 @@ global vH
 global vL
 
 
+# function used in mouse callback to print out HSV values
 def print_HSV(event, x, y, flags, param):
     global mouseX, mouseY
     if event == cv.EVENT_LBUTTONDBLCLK:
@@ -19,16 +20,19 @@ def print_HSV(event, x, y, flags, param):
         print(hsv[mouseY][mouseX])
 
 
+# do nothing function to pass in when making the trackbars
 def nothing(x):
     pass
 
 
+# starts video capture and creates 3 windows
 cap = cv.VideoCapture(0)
 cv.namedWindow("HSV Video")
 cv.namedWindow("Track Bar")
 cv.namedWindow("Tracker")
 cv.setMouseCallback('HSV Video', print_HSV)
 
+# Create track bars for HSV value ranges
 cv.createTrackbar("Hue High", "Track Bar", 0, 179, nothing)
 cv.createTrackbar("Hue Low", "Track Bar", 0, 179, nothing)
 cv.createTrackbar("Saturation High", "Track Bar", 0, 255, nothing)
@@ -42,12 +46,14 @@ while True:
 
     hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 
+    # shows hsv video
     cv.imshow("HSV Video", hsv)
 
     k = cv.waitKey(1)
     if k == 27:
         break
 
+    # creates track bars and sets values to global high and lows for each HSV
     hH = cv.getTrackbarPos("Hue High", "Track Bar")
     hL = cv.getTrackbarPos("Hue Low", "Track Bar")
     sH = cv.getTrackbarPos("Saturation High", "Track Bar")
@@ -55,13 +61,17 @@ while True:
     vH = cv.getTrackbarPos("Value High", "Track Bar")
     vL = cv.getTrackbarPos("Value Low", "Track Bar")
 
+    # creates numpy arrays for low and high values
     max_HSV = np.array([hH, sH, vH])
     low_HSV = np.array([hL, sL, vL])
 
+    # thresholds low and high values then uses a mask to erode and dilate the black and white image
     frame_threshold = cv.inRange(hsv, low_HSV, max_HSV)
     kernel = np.ones((5, 5), np.uint8)
     erosion = cv.erode(frame_threshold, kernel, iterations=1)
     dilation = cv.dilate(erosion, kernel, iterations=1)
+
+    # shows tracked object from HSV trackbar values after being eroded and dilated
     cv.imshow("Tracker", dilation)
 
 cv.destroyAllWindows()
--- Part2.py
+++ Part2.py
@@ -1,29 +1,35 @@
 import cv2 as cv
 import numpy as np
 
+# starts video capture and creates blank variables to be used from step 2
 cap = cv.VideoCapture(0)
 status, frame = cap.read()
 width, height, channels = frame.shape
 bw = np.zeros((width, height, 1), np.uint8)
 imageForAve = np.zeros((width, height, 3), np.float32)
-bright = 35 * np.ones((width, height, 3), np.uint8)
 runningColorDepth = frame.copy()
 difference = frame.copy()
 
-
+print("start")
 while True:
     status, newFrame = cap.read()
 
+    # blurs image then take running average and converts to make same bit as newFrame
     blur = cv.blur(newFrame, (5, 5))
     imageForAve = cv.accumulateWeighted(blur, imageForAve, .4)
     runningColorDepth = cv.convertScaleAbs(imageForAve, runningColorDepth)
+
+    # Take difference of the new frame and running average then turn to grayscale
     difference = cv.absdiff(newFrame, runningColorDepth, difference)
     gray = cv.cvtColor(difference, cv.COLOR_BGR2GRAY)
+
+    # threshold with a low value, blur then threshold with a high value
     ret, thresholdLow = cv.threshold(gray, 10, 255, cv.THRESH_BINARY)
     lowThresh_blur = cv.blur(thresholdLow, (5, 5))
     ret, thresholdHigh = cv.threshold(lowThresh_blur, 250, 255, cv.THRESH_BINARY)
-    contours, hierarchy = cv.findContours(thresholdHigh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
+    # finds contours then thresholds which contours to take, drawing rectangle over contours within threshold
+    contours, hierarchy = cv.findContours(thresholdHigh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
     for contour in contours:
         (x, y, w, h) = cv.boundingRect(contour)
 
@@ -31,6 +37,7 @@ while True:
             continue
         cv.rectangle(newFrame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
+    # shows black and white movement image and contour image that draws rectangles where motion is detected
     cv.imshow("Black and White", thresholdHigh)
     cv.imshow("Contours", newFrame)
 
